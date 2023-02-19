7570f1fffad55e76c8998c7e64fa476452a95faa
Higor Grigorio <higorgrigorio@gmail.com>
2023-02-19 16:13:20 -0300

    feat: conversion of paylod json to sensor credentials array

--- include/sensor-credentials.h
+++ include/sensor-credentials.h
@@ -11,6 +11,9 @@
 #define _SensorCredentials_h_
 
 #include <file.h>
+#include <config/file-sistem.h>
+
+#include <ArduinoJson.h>
 
 /**
  * @brief The credential of a sensor
@@ -25,5 +28,75 @@ struct SensorType
  * @brief The group of credentials of a sensor
  */
 using SensorCredentials = LL<struct SensorType>;
+
+auto CreadentialsFromJson(String json) -> ErrorOr<SensorCredentials>
+{
+    ErrorOr<SensorCredentials> result;
+
+    auto credentials = SensorCredentials();
+    auto doc = DynamicJsonDocument(json.length() + 1);
+
+    auto err = deserializeJson(doc, json);
+
+    if (err != DeserializationError::Ok)
+    {
+        result = failure({
+            .context = "creadentialsFromJson",
+            .message = err.c_str(),
+        });
+    }
+    else if (doc.containsKey("success") && doc["success"].as<bool>())
+    {
+        if (!doc.containsKey("body"))
+        {
+            result = failure({
+                .context = "creadentialsFromJson",
+                .message = "The body is missing",
+            });
+        }
+        else
+        {
+            auto list = SensorCredentials();
+
+            for (auto item : doc["body"].as<JsonArray>())
+            {
+                auto type = item["type"].as<String>();
+                auto id = item["id"].as<String>();
+
+                list.add({type, id});
+            }
+
+            result = ok(list);
+        }
+    }
+    else
+    {
+        result = failure({
+            .context = "creadentialsFromJson",
+            .message = (doc.containsKey("message") ? doc["message"].as<const char *>() : "Unknown error"),
+        });
+    }
+
+    return result;
+}
+
+auto GetSensorCredentials() -> ErrorOr<SensorCredentials>
+{
+    auto result = ErrorOr<SensorCredentials>();
+
+    if (!FileExists(SELF_FILE))
+    {
+        result = failure({
+            .context = "GetSensorCredentials",
+            .message = "The file does not exists",
+        });
+    }
+    else
+    {
+        // TODO: read the file
+    }
+
+    return result;
+}
 
 #endif // ! _SensorCredentials_h_
